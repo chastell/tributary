163c756fdfbfe8d5de02e68c351a21ef1f5ecba5
Piotr Szotkowski <chastell@chastell.net>
2010-07-15 13:34:16 +0200

    refactor Stream spec

--- spec/tributary/stream_spec.rb
+++ spec/tributary/stream_spec.rb
@@ -1,10 +1,17 @@
 module Tributary describe Stream do
 
+  before :all do
+    @about   = Item.new 'spec/fixtures/page/about.md'
+    @battle  = Item.new 'spec/fixtures/article/600.md'
+    @unix    = Item.new 'spec/fixtures/article/unix-millenium-bug.md'
+    @welcome = Item.new 'spec/fixtures/article/welcome.md'
+  end
+
   context '.pick_item' do
 
     it 'returns the relevant Item based on the provided path' do
-      Stream.pick_item('spec/fixtures', 'about').should   == Item.new('spec/fixtures/page/about.md')
-      Stream.pick_item('spec/fixtures', 'welcome').should == Item.new('spec/fixtures/article/welcome.md')
+      Stream.pick_item('spec/fixtures', 'about').should   == @about
+      Stream.pick_item('spec/fixtures', 'welcome').should == @welcome
     end
 
   end
@@ -12,10 +19,7 @@ module Tributary describe Stream do
   context '.recent' do
 
     it 'returns published Items, newest-first' do
-      Stream.recent('spec/fixtures').should == [
-        Item.new('spec/fixtures/article/600.md'),
-        Item.new('spec/fixtures/article/welcome.md'),
-      ]
+      Stream.recent('spec/fixtures').should == [@battle, @welcome]
     end
 
   end
