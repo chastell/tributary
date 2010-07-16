051f3f4bd1d89ddd2dcee7e4c3bb7dc5210f08ff
Piotr Szotkowski <chastell@chastell.net>
2010-07-16 07:54:42 +0200

    new settings: author and url

--- config.ru
+++ config.ru
@@ -1,8 +1,10 @@
 require 'lib/tributary'
 
 Tributary::App.configure do |config|
+  config.set :author,   'Ary Tribut'
   config.set :root,     'spec/fixtures'
   config.set :sitename, 'a tributary site'
+  config.set :url,      'http://tributary.example.net/'
 end
 
 run Tributary::App
--- spec/tributary/app_spec.rb
+++ spec/tributary/app_spec.rb
@@ -6,8 +6,10 @@ module Tributary describe App do
 
   def app
     App.configure do |config|
+      config.set :author,   'Ary Tribut'
       config.set :root,     'spec/fixtures'
       config.set :sitename, 'a tributary site'
+      config.set :url,      'http://tributary.example.net/'
     end
     App
   end
