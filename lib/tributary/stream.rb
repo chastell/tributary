6685f0afeee1305b825da76a06f4f5109cc896bc
Piotr Szotkowski <chastell@chastell.net>
2010-07-22 15:54:24 +0200

    factor out Stream#items_ltd

--- lib/tributary/stream.rb
+++ lib/tributary/stream.rb
@@ -1,29 +1,34 @@
 module Tributary class Stream
 
   def initialize
-    @all_items  = Dir["#{App.root}/*/*.md"].map { |file| Item.new file }.sort
     @items      = Dir["#{App.root}/*/*.md"].map { |file| Item.new file }.sort
-    @items.delete_if { |item| item.lang and not App.lang_limit.include? item.lang } if App.lang_limit and not App.lang_limit.empty?
-    @items      = @items.select { |item| item == @items.find { |i| i.path == item.path } }
     @previous   = Hash[recent.each_cons(2).to_a]
     @subsequent = Hash[recent.reverse.each_cons(2).to_a]
   end
 
   def pick_item path
     path, lang = path.split '.'
-    (lang ? @all_items.select { |item| item.lang == lang } : @items).find { |item| item.path == path }
+    (lang ? @items.select { |item| item.lang == lang } : items_ltd).find { |item| item.path == path }
   end
 
   def previous item
-    @previous[@items.find { |i| i.path == item.path }]
+    @previous[items_ltd.find { |i| i.path == item.path }]
   end
 
   def recent limit = @items.size
-    @items.select(&:published?).take limit
+    items_ltd.select(&:published?).take limit
   end
 
   def subsequent item
-    @subsequent[@items.find { |i| i.path == item.path }]
+    @subsequent[items_ltd.find { |i| i.path == item.path }]
+  end
+
+  private
+
+  def items_ltd
+    items_ltd = @items.dup
+    items_ltd.delete_if { |item| item.lang and not App.lang_limit.include? item.lang } if App.lang_limit and not App.lang_limit.empty?
+    items_ltd.select { |item| item == items_ltd.find { |i| i.path == item.path } }
   end
 
 end end
