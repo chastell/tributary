eed16876751a016a7699e7081432612728d452d1
Piotr Szotkowski <chastell@chastell.net>
2010-07-25 08:14:48 +0200

    structure the App spec a bit

--- spec/tributary/app_spec.rb
+++ spec/tributary/app_spec.rb
@@ -5,182 +5,205 @@ module Tributary describe App do
   include Rack::Test::Methods
 
   def app
+    App
+  end
+
+  before :all do
     App.configure do |config|
       config.set :author,   'Ary Tribut'
       config.set :root,     'spec/site'
       config.set :sitename, 'a tributary site'
     end
-    App
   end
 
-  it 'renders the index view when no path given' do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include 'a tributary index'
+  context 'rendering content' do
+
+    it 'renders the index view when no path given' do
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include 'a tributary index'
+    end
+
+    it 'renders the given view’s index' do
+      get '/articles'
+      last_response.should be_ok
+      last_response.body.should     include 'welcome to tributary'
+      last_response.body.should_not include 'a…'
+    end
+
+    it 'renders the relevant, path-based view' do
+      get '/about'
+      last_response.should be_ok
+      last_response.body.should include 'a tributary page'
+      last_response.body.should include 'tributary <em>about</em> page'
+
+      get '/welcome'
+      last_response.should be_ok
+      last_response.body.should include 'a tributary article'
+      last_response.body.should include 'tributary <em>welcome</em> article'
+    end
+
   end
 
-  it 'renders the relevant, path-based view' do
-    get '/about'
-    last_response.should be_ok
-    last_response.body.should include 'a tributary page'
-    last_response.body.should include 'tributary <em>about</em> page'
+  context 'view settings' do
 
-    get '/welcome'
-    last_response.should be_ok
-    last_response.body.should include 'a tributary article'
-    last_response.body.should include 'tributary <em>welcome</em> article'
+    it 'sets @item.path in the views’ indices' do
+      get '/articles'
+      last_response.should be_ok
+      last_response.body.should include '<title>a tributary site: articles</title>'
+    end
+
+    it 'sets /’s view to :index' do
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include 'index.css'
+    end
+
+    it 'exposes settings to views' do
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include '<title>a tributary site</title>'
+    end
+
+    it 'exposes Stream to views' do
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include 'welcome to tributary'
+
+      get '/600'
+      last_response.should be_ok
+      last_response.body.should include 'bilinguality'
+    end
+
   end
 
-  it 'renders the given view’s index' do
-    get '/articles'
-    last_response.should be_ok
-    last_response.body.should     include 'welcome to tributary'
-    last_response.body.should_not include 'a…'
+  context 'feeds' do
+
+    it 'renders the Atom feed' do
+      get '/index.xml'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'application/atom+xml'
+      last_response.body.should == File.read('spec/fixtures/index.xml')
+    end
+
+    it 'renders per-locale Atom feeds' do
+      get '/index.en.xml'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'application/atom+xml'
+      last_response.body.should == File.read('spec/fixtures/index.en.xml')
+
+      get '/index.pl.xml'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'application/atom+xml'
+      last_response.body.should == File.read('spec/fixtures/index.pl.xml')
+    end
+
+    it 'renders lang_limit-ed Atom feeds' do
+      get '/index.en.en.xml'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'application/atom+xml'
+      last_response.body.should == File.read('spec/fixtures/index.en.en.xml')
+
+      get '/index.pl.pl.xml'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'application/atom+xml'
+      last_response.body.should == File.read('spec/fixtures/index.pl.pl.xml')
+
+      get '/index.en.en+pl.xml'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'application/atom+xml'
+      last_response.body.should == File.read('spec/fixtures/index.en.en+pl.xml')
+    end
+
   end
 
-  it 'sets @item.path in the views’ indices' do
-    get '/articles'
-    last_response.should be_ok
-    last_response.body.should include '<title>a tributary site: articles</title>'
+  context 'stylesheets' do
+
+    it 'renders the CSS stylesheet' do
+      get '/layout.css'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'text/css'
+      last_response.body.should == File.read('spec/fixtures/layout.css')
+    end
+
+    it 'renders per-view CSS stylesheets' do
+      get '/pages.css'
+      last_response.should be_ok
+      last_response.headers['Content-Type'].should == 'text/css'
+      last_response.body.should == File.read('spec/fixtures/pages.css')
+    end
+
   end
 
-  it 'sets /’s view to :index' do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include 'index.css'
-  end
+  context 'localisation and multilinguality' do
 
-  it 'exposes settings to views' do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include '<title>a tributary site</title>'
-  end
+    it 'defaults to nil locale and lang_limit' do
+      App.locale.should     == nil
+      App.lang_limit.should == nil
+    end
 
-  it 'exposes Stream to views' do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include 'welcome to tributary'
+    it 'sets the right App settings and redirects properly' do
+      get '/set?locale=pl', {}, 'HTTP_REFERER' => '/bilingual'
+      last_response.location.should == '/bilingual'
+      follow_redirect!
+      App.locale.should == 'pl'
+      last_response.should be_ok
+      last_response.body.should include '<title>dwujęzyczność</title>'
 
-    get '/600'
-    last_response.should be_ok
-    last_response.body.should include 'bilinguality'
-  end
+      get '/set?lang_limit=pl'
+      follow_redirect!
+      App.lang_limit.should == ['pl']
+      last_response.should be_ok
+      last_response.body.should_not include '600th anniversary (English)'
 
-  it 'renders the Atom feed' do
-    get '/index.xml'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/atom+xml'
-    last_response.body.should == File.read('spec/fixtures/index.xml')
-  end
+      get '/set?locale'
+      follow_redirect!
+      App.locale.should == nil
 
-  it 'renders per-locale Atom feeds' do
-    get '/index.en.xml'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/atom+xml'
-    last_response.body.should == File.read('spec/fixtures/index.en.xml')
+      get '/set?lang_limit'
+      follow_redirect!
+      App.lang_limit.should == nil
+    end
 
-    get '/index.pl.xml'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/atom+xml'
-    last_response.body.should == File.read('spec/fixtures/index.pl.xml')
-  end
+    it 'localises the output based on locale (defaulting to English)' do
+      get '/set?locale=pl'
+      get '/bilingual'
+      last_response.should be_ok
+      last_response.body.should include 'opublikowany 15 lipca 2010'
 
-  it 'renders lang_limit-ed Atom feeds' do
-    get '/index.en.en.xml'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/atom+xml'
-    last_response.body.should == File.read('spec/fixtures/index.en.en.xml')
+      get '/set?locale=en'
+      get '/bilingual'
+      last_response.should be_ok
+      last_response.body.should include 'published 15th of July, 2010'
 
-    get '/index.pl.pl.xml'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/atom+xml'
-    last_response.body.should == File.read('spec/fixtures/index.pl.pl.xml')
+      get '/set?locale'
+      get '/bilingual'
+      last_response.should be_ok
+      last_response.body.should include 'published 15th of July, 2010'
+    end
 
-    get '/index.en.en+pl.xml'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/atom+xml'
-    last_response.body.should == File.read('spec/fixtures/index.en.en+pl.xml')
-  end
+    it 'sets the right value for the <html> element’s lang attribute' do
+      get '/set?locale=en'
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include "<html lang='en'>"
 
-  it 'renders the CSS stylesheet' do
-    get '/layout.css'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'text/css'
-    last_response.body.should == File.read('spec/fixtures/layout.css')
-  end
+      get '/set?locale=pl'
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include "<html lang='pl'>"
 
-  it 'renders per-view CSS stylesheets' do
-    get '/pages.css'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'text/css'
-    last_response.body.should == File.read('spec/fixtures/pages.css')
-  end
+      get '/set?locale'
+      get '/'
+      last_response.should be_ok
+      last_response.body.should include "<html lang='en'>"
 
-  it 'defaults to nil locale and lang_limit' do
-    App.locale.should     == nil
-    App.lang_limit.should == nil
-  end
+      get '/set?locale=en'
+      get '/bilingual.pl'
+      last_response.should be_ok
+      last_response.body.should include "<html lang='pl'>"
+    end
 
-  it 'sets the right App settings and redirects properly' do
-    get '/set?locale=pl', {}, 'HTTP_REFERER' => '/bilingual'
-    last_response.location.should == '/bilingual'
-    follow_redirect!
-    App.locale.should == 'pl'
-    last_response.should be_ok
-    last_response.body.should include '<title>dwujęzyczność</title>'
-
-    get '/set?lang_limit=pl'
-    follow_redirect!
-    App.lang_limit.should == ['pl']
-    last_response.should be_ok
-    last_response.body.should_not include '600th anniversary (English)'
-
-    get '/set?locale'
-    follow_redirect!
-    App.locale.should == nil
-
-    get '/set?lang_limit'
-    follow_redirect!
-    App.lang_limit.should == nil
-  end
-
-  it 'localises the output based on locale (defaulting to English)' do
-    get '/set?locale=pl'
-    get '/bilingual'
-    last_response.should be_ok
-    last_response.body.should include 'opublikowany 15 lipca 2010'
-
-    get '/set?locale=en'
-    get '/bilingual'
-    last_response.should be_ok
-    last_response.body.should include 'published 15th of July, 2010'
-
-    get '/set?locale'
-    get '/bilingual'
-    last_response.should be_ok
-    last_response.body.should include 'published 15th of July, 2010'
-  end
-
-  it 'sets the right value for the <html> element’s lang attribute' do
-    get '/set?locale=en'
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include "<html lang='en'>"
-
-    get '/set?locale=pl'
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include "<html lang='pl'>"
-
-    get '/set?locale'
-    get '/'
-    last_response.should be_ok
-    last_response.body.should include "<html lang='en'>"
-
-    get '/set?locale=en'
-    get '/bilingual.pl'
-    last_response.should be_ok
-    last_response.body.should include "<html lang='pl'>"
   end
 
 end end
